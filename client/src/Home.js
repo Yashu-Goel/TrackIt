cc3dbcd03bbbda880837e85ebffb81b8a9d84a34
Yashu <yashugoeldpsi@gmail.com>
2023-10-06 16:13:54 +0530

    home css fixed

--- client/src/Home.js
+++ client/src/Home.js
@@ -9,20 +9,26 @@ import txtlogo from './onlyTextBlk.png'
 const Home = () => {
   return (
     <div>
-      <Navbar/>
-      <div className='forflex'>
-      <div class="main-home">
-        <img src={logo1} className='oneone'></img>
-        <img src={txtlogo} className='twotwo'></img>
-        <h4 className='three'>Centralised Reporting and Identification of Disease System</h4>
+      <Navbar />
+      <div className="forflex">
+        <div class="main-home">
+              <img src={txtlogo} className="twotwo"></img>
+              <h4 className="three">
+                Centralised Reporting and Identification of Disease System
+              </h4>
+        </div>
+
         <div class="four">
-        <Link to="/patient_auth"><button className='but-home'>Patient</button></Link>
-        <Link to="/doctor_auth"><button className='but-home'>Doctor</button></Link>
+          <Link to="/patient_auth">
+            <button className="but-home">Patient</button>
+          </Link>
+          <Link to="/doctor_auth">
+            <button className="but-home">Doctor</button>
+          </Link>
         </div>
       </div>
-      </div>
     </div>
-  )
+  );
 }
 
 export default Home
